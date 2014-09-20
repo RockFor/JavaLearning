744d4e69adde10753c2956eb38aca3e13e7afcdb
RockFor <igor90bond@gmail.com>
2014-09-20 10:16:06 +0500

    First commit from Idea Jetbrains IDE

--- src/AccessTest.java
+++ src/AccessTest.java
@@ -1,5 +1,28 @@
 /**
- * Created by igor on 20.09.14.
+ * Created by Админ on 17.09.2014.
  */
+class Test2 {
+    int a;          // Доступ определен по умолчанию
+    public int b;   // Открытый доступ
+    private int c;  // Закрытый доступ
+
+    // Method access to set the c variable
+    void setc(int i) {
+        c = i;      // Set c variable
+    }
+    int getc() {
+        return c;   // Get c variable
+    }
+}
 public class AccessTest {
+    public static void main(String[] args) {
+        Test2 ob = new Test2();     // Create a object for the class
+
+        ob.a = 10;  // Имеет прямой доступ
+        ob.b = 20;  // Имеет прямой доступ
+        //ob.c = 30;  // Ошибка
+        System.out.println("ob.a = " + ob.a + " ob.b = " + ob.b);
+        ob.setc(30);
+        System.out.println("ob.c = " + ob.getc());
+    }
 }
